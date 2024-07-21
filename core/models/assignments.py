1a4082c280241ec7abd4d1fb010b56eb327c2346
souravrjpt <souravrajput2760@gmail.com>
2024-07-22 00:40:18 +0530

    code changes for test and yml file

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -64,10 +64,12 @@ class Assignment(db.Model):
     @classmethod
     def submit(cls, _id, teacher_id, auth_principal: AuthPrincipal):
         assignment = Assignment.get_by_id(_id)
-        assertions.assert_found(assignment, 'No assignment with this id was found')
-        assertions.assert_valid(assignment.state != AssignmentStateEnum.DRAFT, 'only a draft assignment can be submitted')
+
         assertions.assert_valid(assignment.student_id == auth_principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'Assignment with empty content cannot be submitted')
+
+        if(assignment):
+            assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, 'only a draft assignment can be submitted')
 
         assignment.teacher_id = teacher_id
         assignment.state = AssignmentStateEnum.SUBMITTED
